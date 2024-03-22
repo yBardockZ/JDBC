ade1be243ff89922cf3f3bf3a1f8092eace3b0ae
Thalles <thalles_leopoldino@outlook.com>
2024-03-22 15:40:14 -0300

    Classe DB e excessão DBException criadas, arquivo db.properties definido

---
+++ src/db/DB.java
@@ -0,0 +1,59 @@
+package db;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+	
+	private static Connection conn = null;
+	
+	public static Connection getConnection() {
+		if (conn != null) {
+			return conn;
+		}
+		
+		else {
+			
+			try {
+				
+				Properties props = new Properties();
+				FileInputStream fileInputStream = new FileInputStream(new File("db.properties"));
+				
+				props.load(fileInputStream);
+				
+				conn = DriverManager.getConnection(props.getProperty("dburl"), props.getProperty("user"),
+						props.getProperty("password"));
+				
+			} catch (SQLException e) {
+				throw new DBException(e.getMessage());
+			} catch (Exception e) {
+				System.out.println("Error: " + e.getMessage());
+				e.printStackTrace();
+			}
+			
+			return conn;
+			
+		}
+		
+	}
+	
+	public static void closeConnection() {
+		try {
+			conn.close();
+			
+		} catch (SQLException e) {
+			throw new DBException(e.getMessage());
+			
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+			e.printStackTrace();
+			
+		}
+		
+	}
+
+}
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module ex064 {
+	requires java.sql;
 }
