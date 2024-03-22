9b2aee10b17a634a28835ad747b8b4bc6d73fa7f
Thalles <thalles_leopoldino@outlook.com>
2024-03-22 16:38:41 -0300

    Classes Department e Seller implementadas

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/model/entities/Seller.java
@@ -0,0 +1,96 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Seller implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String name;
+	private String email;
+	private LocalDate birthDate;
+	private Double baseSalary;
+	
+
+	public Seller() {
+	}
+
+
+	public Seller(Integer id, String name, String email, LocalDate birthDate, Double baseSalary) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+		this.baseSalary = baseSalary;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
+				+ baseSalary + "]";
+	}
+	
+	
+
+}
